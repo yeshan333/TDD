1d66ad1be72f013cc5113ed9af6b0339992caa95
yeshan333 <1329441308@qq.com>
2019-12-21 15:46:33 +0800

    evil version

--- functional tests/superlists/lists/tests.py
+++ functional tests/superlists/lists/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+# from django.core.urlresolvers import resolve # low version
 
-# Create your tests here.
+
+""" # Create your tests here.
 class SmokeTest(TestCase):
     def test_bad_maths(self):
         self.assertEqual(1+1, 3, 'bad calculate!')
+"""
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  # 解析URL，将其映射到相应的视图函数上
+        self.assertEqual(found.func, home_page)
