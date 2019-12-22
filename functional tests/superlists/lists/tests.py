cab38c0e475a4ef9a9a06a25b556a2d7c72fb035
yeshan333 <1329441308@qq.com>
2019-12-22 15:23:23 +0800

    add README.md，wait。。。:sweat_smile:

--- functional tests/superlists/lists/tests.py
+++ functional tests/superlists/lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 # from django.core.urlresolvers import resolve # low version
@@ -21,6 +22,14 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+"""
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+"""
--- functional tests/superlists/lists/views.py
+++ functional tests/superlists/lists/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 # home_page = None
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    # return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
