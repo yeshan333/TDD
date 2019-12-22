77848072c91fd5ff3b8f0307c4984390e52f45a4
yeshan333 <1329441308@qq.com>
2019-12-22 15:28:52 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+## TDD experience
+
+functional tests：functional_tests.py
+
+unittest：functional tests\superlists\lists\tests.py
+
+## Usage
+
+>Django version：3.0.1
+
+```
+# 1、unitest
+cd funtional tests/superlists
+python manage.py test
+# 2、run django server
+python manage.py runserver
+# 3、functional tests
+cd ..
+python functional_tests.py
+```
--- functional tests/functional_tests.py
+++ functional tests/functional_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # nice pratice
 """
@@ -28,9 +29,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # 标题测试
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finished the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # 模拟输入
+        inputbox.send_keys('Buy peacook feathers')
+        inputbox.send_keys(Keys.ENTER)  # 回车
+
+        # 检测是否成功提交
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacook feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
+        # nice
+        self.fail('Finished the test!')
+
+
 
 if __name__ == "__main__":
     # unittest.main()
